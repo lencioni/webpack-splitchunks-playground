2dafe8d128daebb31b072b1da3a20ca442042088
Joe Lencioni <joe.lencioni@airbnb.com>
2018-03-12 18:26:56 -0700

    Add dynamic import to a.bundle

--- src/bundles/a.js
+++ src/bundles/a.js
@@ -5,3 +5,4 @@ import 'aphrodite'; // -> a.bundle
 import '../core-module'; // -> core.bundle
 import '../core-module-b'; // -> coreB.bundle
 import '../non-core-module'; // -> a.bundle
+import(/* webpackChunkName: "FOO" */ '../non-core-module-b'); // -> FOO.bundle
